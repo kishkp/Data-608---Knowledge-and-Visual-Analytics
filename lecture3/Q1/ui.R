f4862e61b67e11a28ed87904a65e4d4de3c51ba2
kishkp <kishkp@yahoo.com>
2017-04-22 17:00:17 +0400

    cleanup

--- lecture3/Q1/ui.R
+++ lecture3/Q1/ui.R
@@ -18,56 +18,5 @@ fluidPage(
                       tabPanel("Vegalite", vegaliteOutput('plot4'))
                     )
         )
-  )
+)
 
-# fluidPage(
-#   headerPanel('Mortality Rate'),
-#   sidebarPanel(
-#     selectInput('year', 'Year', unique(df$Year), selected='2010'),
-#     selectInput('disease', 'Disease', unique(df$ICD.Chapter), selected='Neoplasms')
-#   ),
-#   mainPanel(
-#     tabsetPanel(tabPanel("GGPlot",     plotOutput('plot1')),
-#                   tabPanel("GoogleVis", htmlOutput('plot2')), 
-#                   tabPanel("Plotly", plotlyOutput('plot3'))
-#                 # tabPanel("Vegalite", plotOutput('plot1'))
-#                 )
-#     )
-# )
-=======
-fluidPage(
-  headerPanel('Mortality Rate'),
-  # sidebarPanel(
-  #   selectInput('year', 'Year', unique(df$Year), selected='2010'),
-  #   selectInput('disease', 'Disease', unique(df$ICD.Chapter), selected='Neoplasms')
-  # ),
-  mainPanel(
-    fluidRow(
-      column(3,
-             selectInput('year', 'Year', unique(df$Year), selected='2010')),
-      column(9,
-             selectInput('disease', 'Disease', unique(df$ICD.Chapter), selected='Neoplasms'))
-    ),
-
-        tabsetPanel(tabPanel("GGPlot",     plotOutput('plot1')),
-                      tabPanel("GoogleVis", htmlOutput('plot2')),
-                      tabPanel("Plotly", plotlyOutput('plot3')),
-                      tabPanel("Vegalite", vegaliteOutput('plot4'))
-                    )
-        )
-  )
-
-# fluidPage(
-#   headerPanel('Mortality Rate'),
-#   sidebarPanel(
-#     selectInput('year', 'Year', unique(df$Year), selected='2010'),
-#     selectInput('disease', 'Disease', unique(df$ICD.Chapter), selected='Neoplasms')
-#   ),
-#   mainPanel(
-#     tabsetPanel(tabPanel("GGPlot",     plotOutput('plot1')),
-#                   tabPanel("GoogleVis", htmlOutput('plot2')), 
-#                   tabPanel("Plotly", plotlyOutput('plot3'))
-#                 # tabPanel("Vegalite", plotOutput('plot1'))
-#                 )
-#     )
-# )
--- lecture3/Q2/ui.R
+++ lecture3/Q2/ui.R
@@ -18,25 +18,3 @@ fluidPage(
                     )
         )
   )
-=======
-fluidPage(
-  headerPanel('Mortality Rate'),
-  # sidebarPanel(
-  #   selectInput('year', 'Year', unique(df$Year), selected='2010'),
-  #   selectInput('disease', 'Disease', unique(df$ICD.Chapter), selected='Neoplasms')
-  # ),
-  mainPanel(
-    fluidRow(
-      column(6,
-             selectInput("State", "State: (Multi Select)", unique(df$State), selected = "Nat Avg", multiple = TRUE)),
-      column(6,
-             selectInput('disease', 'Disease', unique(df$ICD.Chapter), selected='Neoplasms'))
-    ),
-      tabsetPanel(tabPanel("GGPlot",     plotOutput('plot1')),
-                      tabPanel("GoogleVis", htmlOutput('plot2')),
-                      tabPanel("Plotly", plotlyOutput('plot3')),
-                      tabPanel("Vegalite", vegaliteOutput('plot4'))
-                    )
-        )
-  )
-
