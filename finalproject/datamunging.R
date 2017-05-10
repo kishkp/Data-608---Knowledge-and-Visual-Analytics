8716b3669dc6917cbd46a6a28379207ecc68e754
kishkp <kishkp@yahoo.com>
2017-05-10 20:04:55 +0400

    data updated

---
+++ finalproject/datamunging.R
@@ -0,0 +1,85 @@
+library(dplyr)
+
+#### WARNING #####
+## This is a huge file of about 300 MB. 
+## Please change the below path to the path where you have the huge data file.
+
+df <- read.csv('D:/CUNY/Courses/Data 608 - Knowledge and Visual Analytics/finalproject/Consumer_Complaints.csv', stringsAsFactors = FALSE)
+
+# Add Month and Year columns
+df$Month <- substr(df[,1],1,2)
+df$Year <- substr(df[,1],7,11)
+
+# Retain only required columns
+reqCols <- c("Month", "Year", "Product", "Company.public.response", "Company", "State", "Submitted.via", "Company.response.to.consumer", "Timely.response.", "Consumer.disputed.")
+df <- df[, reqCols]
+
+# Rename Columns
+colnames(df) <- c("Month", "Year", "Product", "CompPubResp","Company","State","Channel","Status",	"Timely", "Disputed")
+
+# Collapse products with less than 1% into "Others"
+product_arr <- df %>% group_by(Product) %>% count() %>% mutate(pert = n/sum(n)*100)
+others <- as.character(unlist(product_arr[product_arr$pert < 1,1]))
+df[df$Product %in% others, c("Product")] <- "Others"
+
+# Collapse Company with less than 1% into "Others"
+company_arr <- df %>% group_by(Company) %>% count() %>% mutate(pert = n/sum(n)*100)
+others <- as.character(unlist(company_arr[company_arr$pert < 1,1]))
+df[df$Company %in% others, c("Company")] <- "Others"
+
+# Collapse rows and get counts 
+df_new <- df %>% 
+          group_by(Month, Year, Product, CompPubResp, Company, State, Channel, Status, Timely, Disputed) %>% 
+          summarise("Complaints"=n())
+
+
+##### Condense the huge dataset by converting all character variables to 'numeric codes'.
+
+# Product
+Product_names <- as.character(levels(factor(df_new$Product)))
+df_new$Product_codes <- as.numeric(factor(df_new$Product))
+
+# CompPubResp
+CompPubResp_names <- as.character(levels(factor(df_new$CompPubResp)))
+df_new$CompPubResp_codes <- as.numeric(factor(df_new$CompPubResp))
+
+# Company
+Company_names <- as.character(levels(factor(df_new$Company)))
+df_new$Company_codes <- as.numeric(factor(df_new$Company))
+
+# State
+State_names <- as.character(levels(factor(df_new$State)))
+df_new$State_codes <- as.numeric(factor(df_new$State))
+
+# Channel
+Channel_names <- as.character(levels(factor(df_new$Channel)))
+df_new$Channel_codes <- as.numeric(factor(df_new$Channel))
+
+# Status
+Status_names <- as.character(levels(factor(df_new$Status)))
+df_new$Status_codes <- as.numeric(factor(df_new$Status))
+
+# Timely
+Timely_names <- as.character(levels(factor(df_new$Timely)))
+df_new$Timely_codes <- as.numeric(factor(df_new$Timely))
+
+# Disputed
+Disputed_names <- as.character(levels(factor(df_new$Disputed)))
+df_new$Disputed_codes <- as.numeric(factor(df_new$Disputed))
+
+######### Generate the coded dataset
+
+reqCols <- c("Month", "Year", "Product_codes", "CompPubResp_codes", "Company_codes", 
+             "State_codes", "Channel_codes", "Status_codes", "Timely_codes", "Disputed_codes")
+
+df_coded <- df_new[,reqCols]
+
+# Reset names of column
+colnames(df) <- c("Month", "Year", "Product", "CompPubResp","Company","State","Channel","Status",	"Timely", "Disputed")
+
+# Write the condensed csv back to the disk
+# This file has been uploaded to github to be used for rest of the analysis.
+
+write.csv(df_coded, 'D:/CUNY/Courses/Data 608 - Knowledge and Visual Analytics/finalproject/Consumer_Complaints_coded.csv', row.names = FALSE)
+
+
--- finalproject/server.R
+++ finalproject/server.R
@@ -9,30 +9,41 @@ library(vegalite)
 library(reshape2)
 library(data.table)
 
-df <- read.csv('https://raw.githubusercontent.com/kishkp/Data-608---Knowledge-and-Visual-Analytics/master/finalproject/Data/Consumer_Complaints_condensed.csv', stringsAsFactors = FALSE)
-
-all_states <- unique(df$State)
-all_products <- c('Bank account or service', 'Credit card', 'Credit reporting', 'Debt collection', 'Mortgage', 'Others')
-all_compresp <- c('Company believes complaint caused principally by actions of third party outside the control or direction of the company', 'Company believes complaint is the result of an isolated error', 'Company believes complaint relates to a discontinued policy or procedure', 'Company believes complaint represents an opportunity for improvement to better serve consumers', 'Company believes it acted appropriately as authorized by contract or law', 'Company believes the complaint is the result of a misunderstanding', 'Company cant verify or dispute the facts in the complaint', 'Company chooses not to provide a public response', 'Company disputes the facts presented in the complaint', 'Company has responded to the consumer and the CFPB and chooses not to provide a public response', 'No Response')
-all_companies <- c('Amex', 'Bank of America', 'Capital One', 'Citibank', 'Discover', 'Ditech Financial LLC', 'Encore Capital Group', 'Equifax', 'Experian', 'HSBC North America Holdings Inc.', 'JPMorgan Chase & Co.', 'Nationstar Mortgage', 'Navient Solutions, LLC.', 'Ocwen', 'Others', 'PNC Bank N.A.', 'Synchrony Financial', 'TransUnion Intermediate Holdings, Inc.', 'U.S. Bancorp', 'Wells Fargo & Company')
-all_channels <-  c('Email', 'Fax', 'Phone', 'Postal mail', 'Referral', 'Web')
-all_status <- c('Closed', 'Closed with explanation', 'Closed with monetary relief', 'Closed with non-monetary relief', 'Closed with relief', 'Closed without relief', 'In progress', 'Untimely response')
-all_years <- unique(df$Year)
 
 
-prod.fact <- factor(df$Product, labels = all_products)
-compresp.fact <- factor(df$CompPubResp, labels = all_compresp)
-comp.fact <- factor(df$Company, labels = all_companies)
-channel.fact <- factor(df$Channel, labels = all_channels)
-status.fact <- factor(df$Status, labels = all_status)
+df <- read.csv('https://raw.githubusercontent.com/kishkp/Data-608---Knowledge-and-Visual-Analytics/master/finalproject/Data/Consumer_Complaints_coded.csv', stringsAsFactors = FALSE)
 
-df <- cbind.data.frame(select(df, Month, Year, State, Timely, Disputed, Complaints), Product = prod.fact, CompPubResp = compresp.fact, Company = comp.fact, Channel = channel.fact, Status = status.fact)
+# "Uncode"  the columns of the condensed dataset
+products <- c('Bank account or service', 'Consumer Loan', 'Credit card', 'Credit reporting', 'Debt collection', 'Mortgage', 'Others', 'Student Loan')
+compresp <- c('No response', 'Company believes complaint caused principally by actions of third party outside the control or direction of the company', 'Company believes complaint is the result of an isolated error', 'Company believes complaint relates to a discontinued policy or procedure', 'Company believes complaint represents an opportunity for improvement to better serve consumers', 'Company believes it acted appropriately as authorized by contract or law',  'Company believes the complaint is the result of a misunderstanding', 'Company cant verify or dispute the facts in the complaint', 'Company chooses not to provide a public response', 'Company disputes the facts presented in the complaint', 'Company has responded to the consumer and the CFPB and chooses not to provide a public response')
+companies <- c('Amex', 'Bank of America', 'Capital One', 'Citibank', 'Ditech Financial LLC', 'Encore Capital Group', 'Equifax', 'Experian', 'JPMorgan Chase & Co.', 'Nationstar Mortgage', 'Navient Solutions, LLC.', 'Ocwen', 'Others', 'PNC Bank N.A.', 'Synchrony Financial', 'TransUnion Intermediate Holdings, Inc.', 'U.S. Bancorp', 'Wells Fargo & Company')
+states <- c("", "AA", "AE", "AK", "AL", "AP", "AR", "AS", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "FM", "GA", "GU", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MH", "MI", "MN", "MO", "MP", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "PR", "PW", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VI", "VT", "WA", "WI", "WV", "WY")
+channels <-  c('Email', 'Fax', 'Phone', 'Postal mail', 'Referral', 'Web')
+status <- c('Closed', 'Closed with explanation', 'Closed with monetary relief', 'Closed with non-monetary relief', 'Closed with relief', 'Closed without relief', 'In progress', 'Untimely response')
+timely <- c("No", "Yes")
+disputed <- c( "", "No", "Yes")
+
+years <- unique(df$Year)
+
+
+prod.fact <- factor(df$Product, labels = products)
+compresp.fact <- factor(df$CompPubResp, labels = compresp)
+comp.fact <- factor(df$Company, labels = companies)
+states.fact <- factor(df$State, labels = states)
+channel.fact <- factor(df$Channel, labels = channels)
+status.fact <- factor(df$Status, labels = status)
+timely.fact <- factor(df$timely, labels = timely)
+disputed.fact <- factor(df$disputed, labels = disputed)
+
+df <- cbind.data.frame(select(df, Month, Year, Complaints), Product = prod.fact, 
+                       CompPubResp = compresp.fact, Company = comp.fact, State = states.fact, 
+                       Channel = channel.fact, Status = status.fact, Timely = timely.fact, Disputed = disputed.fact)
 
 function(input, output, session) {
   selectedData <- reactive({
     select_cols <- c(input$dim, "Year", "Complaints", "Timely_Count", "Dispute_Count")
     group_cols <- c(input$dim, "Year")
-    CJ_cols <- switch(input$dim, State = all_states, Product = all_products, CompPubResp = all_compresp, Company = all_companies,                   Channel = all_channels, Status=all_status)
+    CJ_cols <- switch(input$dim, State = states, Product = products, CompPubResp = compresp, Company = companies,                   Channel = channels, Status=status)
     
     
     dfSlice <- df %>%
@@ -44,7 +55,7 @@ function(input, output, session) {
       data.table() %>%
       setkeyv(group_cols)
     
-    dfs <- data.frame(dfSlice[CJ(CJ_cols,all_years), roll=TRUE])
+    dfs <- data.frame(dfSlice[CJ(CJ_cols,years), roll=TRUE])
     # dfs[is.na(dfs$Complaints), ]$Complaints <- 0
     # dfs[is.na(dfs$Timely_Count), ]$Timely_Count <- 0
     # dfs[is.na(dfs$Dispute_Count), ]$Dispute_Count <- 0
