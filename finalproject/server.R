2566731fcbe31a42eb30816186928d836e2da24c
kishkp <kishkp@yahoo.com>
2017-05-14 18:33:46 +0400

    changes

--- finalproject/server.R
+++ finalproject/server.R
@@ -10,86 +10,128 @@ library(reshape2)
 library(data.table)
 
 
-
+# Read dataset from GitHub
 df <- read.csv('https://raw.githubusercontent.com/kishkp/Data-608---Knowledge-and-Visual-Analytics/master/finalproject/data/Consumer_Complaints_coded.csv', stringsAsFactors = FALSE)
+
 
 # "Uncode"  the columns of the condensed dataset
 products <- c('Bank account or service', 'Consumer Loan', 'Credit card', 'Credit reporting', 'Debt collection', 'Mortgage', 'Others', 'Student Loan')
 compresp <- c('No response', 'Company believes complaint caused principally by actions of third party outside the control or direction of the company', 'Company believes complaint is the result of an isolated error', 'Company believes complaint relates to a discontinued policy or procedure', 'Company believes complaint represents an opportunity for improvement to better serve consumers', 'Company believes it acted appropriately as authorized by contract or law',  'Company believes the complaint is the result of a misunderstanding', 'Company cant verify or dispute the facts in the complaint', 'Company chooses not to provide a public response', 'Company disputes the facts presented in the complaint', 'Company has responded to the consumer and the CFPB and chooses not to provide a public response')
 companies <- c('Amex', 'Bank of America', 'Capital One', 'Citibank', 'Ditech Financial LLC', 'Encore Capital Group', 'Equifax', 'Experian', 'JPMorgan Chase & Co.', 'Nationstar Mortgage', 'Navient Solutions, LLC.', 'Ocwen', 'Others', 'PNC Bank N.A.', 'Synchrony Financial', 'TransUnion Intermediate Holdings, Inc.', 'U.S. Bancorp', 'Wells Fargo & Company')
-states <- c("", "AA", "AE", "AK", "AL", "AP", "AR", "AS", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "FM", "GA", "GU", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MH", "MI", "MN", "MO", "MP", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "PR", "PW", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VI", "VT", "WA", "WI", "WV", "WY")
+states <- c("", "AA", "AE", "AK", "AL", "AP", "AR", "AS", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "FM", "GA", "GU", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MH", "MI", "MN", "MO", "MP", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "PR", "PW", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VI", "VT", "WA", "WI", "WV", "WY", "LS")
 channels <-  c('Email', 'Fax', 'Phone', 'Postal mail', 'Referral', 'Web')
 status <- c('Closed', 'Closed with explanation', 'Closed with monetary relief', 'Closed with non-monetary relief', 'Closed with relief', 'Closed without relief', 'In progress', 'Untimely response')
-# timely <- c("No", "Yes")
-# disputed <- c( "", "No", "Yes")
-
 years <- unique(df$Year)
 
+# Create factors for each of the dimensions in the data 
 prod.fact <- factor(df$Product, labels = products)
 compresp.fact <- factor(df$CompPubResp, labels = compresp)
 comp.fact <- factor(df$Company, labels = companies)
-states.fact <- factor(df$State, labels = states)
+states.fact <- factor(df$State, levels = as.character(1:64), labels = states)
 channel.fact <- factor(df$Channel, labels = channels)
 status.fact <- factor(df$Status, labels = status)
-# timely.fact <- factor(df$Timely, labels = timely)
-# disputed.fact <- factor(df$Disputed, labels = disputed)
 
-# df$Timely_Count <- if_else(df$Timely=='Yes', 1, 0, missing=0)
-# df$Dispute_Count <- if_else(df$Disputed=='Yes', 1, 0, missing=0)
-  
-  
+# recreate the dataset with the factors instead of the coded values  
 df <- cbind.data.frame(select(df, Month, Year, Complaints, Timely_Count, Disputed_Count), Product = prod.fact, 
                        CompPubResp = compresp.fact, Company = comp.fact, State = states.fact, 
                        Channel = channel.fact, Status = status.fact)
 
-function(input, output, session) {
-  CompaniesTrendData <- reactive({
-    select_cols <- c(input$CT_dim, "Year", "Complaints", "Timely_Count", "Disputed_Count")
-    group_cols <- c(input$CT_dim, "Year")
-    CJ_cols <- switch(input$CT_dim, State = states, Product = products, CompPubResp = compresp, 
-                      Company = companies, Channel = channels, Status=status)
-    
-    
-    dfSlice <- df %>%
-      select_(.dots = select_cols) %>%
-      group_by_(.dots = group_cols) %>% 
-      summarise(Complaints = sum(Complaints), Timely_Count=sum(Timely_Count), Dispute_Count=sum(Disputed_Count)) %>%
-      data.table() %>%
-      setkeyv(group_cols)
-    
-    dfs <- data.frame(dfSlice[CJ(CJ_cols,years), roll=TRUE])
-    # dfs[is.na(dfs$Complaints), ]$Complaints <- 0
-    # dfs[is.na(dfs$Timely_Count), ]$Timely_Count <- 0
-    # dfs[is.na(dfs$Dispute_Count), ]$Dispute_Count <- 0
-  })
+create_hierarchial_JSON <- function(){
+  
+} 
 
-  StateAnalysisData <- reactive({
-    select_prod <- input$AS_Comp
-    select_comp <- input$AS_Prod
+
+function(input, output, session) {
+
+###############################################################  
+
+  # Observe the webpage for changes - when the user selects from the dropdowns
+
+  observe({
+    print(input$tabs)
+    if(input$tabs == "PS") {
+
+    } else if(input$tabs == "CT") {
+
+      # First Analysis - Companies Trend
+      CompaniesTrendData <- reactive({
+        
+        # Select the columns including the column selected in the Radio Buttons.
+        select_cols <- c(input$CT_dim, "Year", "Complaints", "Timely_Count", "Disputed_Count")
+          
+        # Group data by the selected column and year
+        group_cols <- c(input$CT_dim, "Year")
+          
+        # Create a cross join to include all states and all years. 
+        # This is useful to fill in any missing states or years 
+        CJ_cols <- switch(input$CT_dim, State = states, Product = products, CompPubResp = compresp,
+                          Company = companies, Channel = channels, Status=status)
+          
+        # Create the required dataset
+        dfSlice <- df %>%
+          select_(.dots = select_cols) %>%
+          group_by_(.dots = group_cols) %>% 
+          summarise(Complaints = sum(Complaints), Timely_Count=sum(Timely_Count), Disputed_Count=sum(Disputed_Count)) %>%
+          data.table() %>%
+          setkeyv(group_cols)
+        
+        # Ensure that the data has no gaps in rows i.e the rows are padded with missing years and 
+        # states with the values set to NA
+        CT_Data <- data.frame(dfSlice[CJ(CJ_cols,years), roll=TRUE])
+      })
+      
+      output$plot1 <- renderGvis({
+        gvisMotionChart(CompaniesTrendData(), 
+                        idvar=paste0("", input$CT_dim, ""), 
+                        timevar="Year",
+                        xvar = "Timely_Count", yvar = "Disputed_Count",
+                        sizevar = "Complaints",    
+                        options=list(width="700px", height="500px"))
+      })
+      
+    } else if(input$tabs == "AS") {
+
+      ###################################################################  
+      # Second Analysis - State Analysis    
+      # create necessary dataset
+      AS_Data <- df %>%
+        filter(Product == input$AS_Prod, Company == input$AS_Comp) %>%
+        mutate(State=as.character(State)) %>%
+        select(State, Complaints, Timely_Count, Disputed_Count) %>%
+        group_by(State) %>% 
+        summarise(Complaints = sum(Complaints), Timely_Count=sum(Timely_Count), Disputed_Count=sum(Disputed_Count)) %>%
+        data.table() %>%
+        setkey("State")
+      
+      # Ensure that you have all the states i.e pad states that are not existing
+      AS_Data <- data.frame(AS_Data[states,])
+      
+      # set padded states with values of 0
+      AS_Data[is.na(AS_Data)] <- 0
+      
+      # send this revised data back to the javascript so that the JS can render the new chart.
+      session$sendCustomMessage(type = "ASdataChanged", AS_Data)
+
+  } else if(input$tabs == "CP") {
+
+    ###################################################################  
+    # Third Analysis - Company Performance Analysis    
     
-    dfSlice <- df %>%
-      select_(.dots = select_cols) %>%
-      group_by_(.dots = group_cols) %>% 
-      summarise(Complaints = sum(Complaints), Timely_Count=sum(Timely_Count), Dispute_Count=sum(Dispute_Count)) %>%
-      data.table() %>%
-      setkeyv(group_cols)
+    select_cols <- c(input$CP_fact, "Company", "Product", "Channel", "Status")
     
-    dfs <- data.frame(dfSlice[CJ(CJ_cols,years), roll=TRUE])
-    # dfs[is.na(dfs$Complaints), ]$Complaints <- 0
-    # dfs[is.na(dfs$Timely_Count), ]$Timely_Count <- 0
-    # dfs[is.na(dfs$Dispute_Count), ]$Dispute_Count <- 0
-  })
-  
-  
-  
-  output$plot1 <- renderGvis({
-    gvisMotionChart(CompaniesTrendData(), 
-                    idvar=paste0("", input$dim, ""), 
-#                    idvar=paste0("", input$dim, ""), 
-                    timevar="Year",
-                    xvar = "Timely_Count", yvar = "Dispute_Count",
-                    sizevar = "Complaints",    
-                    options=list(width="700px", height="500px")
-    )
-    })
+    # create necessary dataset
+    CP_Data <- df %>%
+      select(Company, Product, Channel, Status, Complaints, Timely_Count, Disputed_Count) %>%
+      group_by(Company, Product, Channel, Status) %>% 
+      summarise(Complaints = sum(Complaints), Timely_Counts=sum(Timely_Count), Disputed_Counts=sum(Disputed_Count)) %>%
+      select_(.dots = select_cols)
+    
+    # send this revised data back to the javascript so that the JS can render the new chart.
+    # session$sendCustomMessage(type = "CPdataChanged", data.table(CP_Data))
+    
+    } else if(input$tabs == "CA") {
+      
+    }  
+    
+  }) 
 }
--- finalproject/ui.R
+++ finalproject/ui.R
@@ -9,23 +9,22 @@ library(vegalite)
 library(reshape2)
 library(data.table)
 
-
 fluidPage(theme = shinytheme("cerulean"),
-  #shinythemes::themeSelector('cerulean'), 
-  navbarPage("Consumer Complaints Analysis",
-    tabPanel("Project Summary", 
+  #shinythemes::themeSelector('cerulean'),
+  navbarPage("Consumer Complaints Analysis", id = "tabs",
+    tabPanel("Project Summary", value = "PS",  
       fluidPage(
         mainPanel(
-          includeHTML("ProjectBrief.html")
+          includeHTML("html/ProjectBrief.html")
         )
       )
     ),
     
-    tabPanel("Complaints Trend",
+    tabPanel("Complaints Trend", value = "CT",
       fluidPage(
         sidebarLayout(
           sidebarPanel(
-            includeHTML("ComplaintsTrend_Brief.html"),
+            includeHTML("html/ComplaintsTrend_Brief.html"),
             radioButtons("CT_dim", "Dimension:",
                          c("State" = "State", "Product" = "Product", "Company Response" = "CompPubResp",
                            "Company" = "Company", "Channel" = "Channel", "Status" = "Status"), selected = "Product"),
@@ -36,34 +35,41 @@ fluidPage(theme = shinytheme("cerulean"),
         )
       )  
     ),
-    tabPanel("Analysis by State",
+    
+    tabPanel("Analysis by State", value = "AS",
       fluidPage(
         sidebarLayout(
           sidebarPanel(
-            includeHTML("AnalysisByState_Brief.html"),
-            selectInput("AS_Comp", "Company :", c('Amex', 'Bank of America', 'Capital One', 'Citibank', 
+            includeHTML("html/AnalysisByState_Brief.html"),
+            selectInput(inputId = "AS_Comp", label = "Company :", choices = c('Amex', 'Bank of America', 'Capital One', 'Citibank', 
                           'Ditech Financial LLC', 'Encore Capital Group', 'Equifax', 'Experian', 'JPMorgan Chase & Co.', 'Nationstar Mortgage', 
                           'Navient Solutions, LLC.', 'Ocwen', 'Others', 'PNC Bank N.A.', 'Synchrony Financial', 
-                          'TransUnion Intermediate Holdings, Inc.', 'U.S. Bancorp', 'Wells Fargo & Company'), TRUE),
-            selectInput("AS_Prod", "product :",  c('Bank account or service', 'Consumer Loan', 'Credit card', 
-                          'Credit reporting', 'Debt collection', 'Mortgage', 'Others', 'Student Loan'), TRUE),
+                          'TransUnion Intermediate Holdings, Inc.', 'U.S. Bancorp', 'Wells Fargo & Company')),
+            selectInput(inputId = "AS_Prod", label = "Product :",  choices = c('Bank account or service', 'Consumer Loan', 'Credit card', 
+                          'Credit reporting', 'Debt collection', 'Mortgage', 'Others', 'Student Loan')),
             width=3),
           mainPanel(
-            includeHTML("AnalysisByState.html")
+            includeHTML("html/AnalysisByState.html")
           )
         )
       )
     ),
-    tabPanel("Company Performance",
+    
+    tabPanel("Company Performance", value = "CP",
       fluidPage(
-        mainPanel(
-          includeHTML("companyperformance.html")
+        sidebarLayout(
+          sidebarPanel(
+            includeHTML("html/CompanyPerformance_Brief.html"),
+            radioButtons("CP_fact", "Measures:",
+                         c("Complaints" = "Complaints", "Timely" = "Timely_Counts", 
+                           "Disputed" = "Disputed_Counts"), selected = "Complaints"),
+            width=3),
+          mainPanel(
+            includeHTML("html/CompanyPerformance.html")
+          )
         )
       )
     ),
-    tabPanel("Channel Analysis"),
-    tabPanel("Response Time Analysis"),
-    tabPanel("Consumer Disputes")
-
+    tabPanel("Channel Analysis", value = "CA")
   )
 )
